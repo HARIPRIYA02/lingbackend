3fb265e1e874a07ddfc91a3f30b6c325dd6611ae
HARIPRIYA02 <haripriyamuppidi02@gmail.com>
2025-06-23 16:37:06 -0400

    lo

--- lingbackend/server.ts
+++ lingbackend/server.ts
@@ -38,5 +38,8 @@ app.get('/leaderboard', async (req, res) => {
   res.json(result.rows);
 });
 
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
