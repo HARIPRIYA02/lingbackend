a8bcafeb4b8d91b671e36cdf66250b5f5d8e9912
HARIPRIYA02 <haripriyamuppidi02@gmail.com>
2025-06-23 10:41:42 -0400

    Initial commit for Render deployment

--- lingbackend/server.ts
+++ lingbackend/server.ts
@@ -39,4 +39,4 @@ app.get('/leaderboard', async (req, res) => {
 });
 
 
-app.listen(5000, '0.0.0.0',() => console.log('Server running on port 5000'));
+app.listen(5000, () => console.log('Server running on port 5000'));
