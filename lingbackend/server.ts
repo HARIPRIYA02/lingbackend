7879db05ecee2f2b7755868d773f006ac2ae9fee
Hari Priya Muppidi <55634286+HARIPRIYA02@users.noreply.github.com>
2025-06-23 23:12:28 -0400

    Update server.ts

--- lingbackend/server.ts
+++ lingbackend/server.ts
@@ -38,7 +38,7 @@ app.get('/leaderboard', async (req, res) => {
   res.json(result.rows);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
